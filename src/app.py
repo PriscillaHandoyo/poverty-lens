64e5a4c01341618552498c4b3ac45f1e4d7d8d55
Priscilla Handoyo <mariapriscilladeanne.handoyo@students.mq.edu.au>
2025-08-15 21:00:21 +1000

    20250815_18 - Fix the UI/UX for the trends - 5

--- src/app.py
+++ src/app.py
@@ -310,7 +310,7 @@ if not row.empty:
         color="Indicator",
         template="plotly_dark",
         markers=True,
-        height=423
+        height=422
     )
     fig_trend.update_layout(
         xaxis_title="Year",
